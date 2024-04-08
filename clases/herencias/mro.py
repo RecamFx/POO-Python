9f9fae55901dba01836651b6d1b4f69bb4775bbc
RecamFx <camiloreartesgm@gmail.com>
2024-04-08 16:44:46 -0300

    Automatic Terminal

--- clases/herencias/mro.py
+++ clases/herencias/mro.py
@@ -4,17 +4,30 @@ class A:
     def hablar(self):
         print("Hablando desde A")
         
+class F:
+    def hablar(self):
+        print("Hablando desde F")
+        
 class B(A):
     def hablar(self):
         print("Hablando desde B")
         
-class C(A):
+class C(F):
     def hablar(self):
         print("Hablando desde C")
         
 class D(B,C):
-    def hablar(self):
-        print("Hablando desde D")
+    pass
+    # def hablar(self):
+    #     print("Hablando desde D")
+
         
 d = D()
+
 d.hablar()
+
+# Si le ponemos pass a D vamos a ver que ya no imprime desde D, va a pasar a B
+# PRIORIDAD:
+# D,B,C,A
+# Porque la b?
+# Porque es la primera que definimos en el atributo de la clase D
