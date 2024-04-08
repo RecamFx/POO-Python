8a9ed4a916d592e8b63d7c73ae668696aaeadfcf
RecamFx <camiloreartesgm@gmail.com>
2024-04-08 16:12:08 -0300

    Automatic Terminal

--- clases/herencias/herenciaMultiple.py
+++ clases/herencias/herenciaMultiple.py
@@ -1,3 +1,5 @@
+# Heredar mas de una clase
+
 class Persona:
     def __init__(self, nombre, edad , nacionalidad):
         self.nombre = nombre 
@@ -19,10 +21,32 @@ class Artista:
         self.habilidad = habilidad
         
     def mostrar_habilidad(self):
-        print(f"La habilidad es {self.habilidad}")
+        return f"La habilidad es {self.habilidad}"
 
 
-roberto = Empleado("Roberto",20,"Argentino","programador",100000) 
+
+class EmpleadoArtista(Persona, Artista):
+    def __init__(self, nombre, edad , nacionalidad, habilidad, salario, empresa): # Tod0s los atributos que quiere eredar
+        Persona.__init__(self, nombre, edad , nacionalidad) # De persona
+        Artista.__init__(self, habilidad) # De artista
+        
+        self.salario = salario
+        self.empresa = empresa
+        self.habilidad = habilidad
+        
+    def mostrar_habilidad(self): # Creamos esta funcion para demostrar lo que hace super()
+        return "No tengo jaja"
+        
+    def presentare(self):
+        return f"{self.mostrar_habilidad()}", f"{super().mostrar_habilidad()}" 
+        # Super() seria mas correcto, ya que le decimos al programa que es una herencia
+        # Si ponemos self, va a buscar en la misma clase y despues en las clases herederas
+        # Super() directamente busca en las clases herederas
+        
+        # Por eso nos da 2 resultados diferentes
+
+roberto = EmpleadoArtista("Roberto",20,"Argentino","Cantar",100000,"ProgramingInc") 
 
 print(roberto.nombre) #? Roberto
-roberto.hablar() #? Roberto esta hablando!
+print(roberto.presentare()[0]) #? No tengo jaja
+print(roberto.presentare()[1]) #? La habilidad es Cantar
