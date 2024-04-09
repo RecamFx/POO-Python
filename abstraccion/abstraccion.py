a7440e79d595e0afa212b1b2abcb06813e0c3c28
RecamFx <camiloreartesgm@gmail.com>
2024-04-09 18:00:00 -0300

    Automatic Terminal

--- abstraccion/abstraccion.py
+++ abstraccion/abstraccion.py
@@ -1,6 +1,8 @@
 # Abstraccion es cuando el usuario no sabe que hacemos en el codigo, es como:
 # Saber manejar un auto pero no saber como se mueve el motor para dar la fuerza de empuje
 
+# Es como ocultar la complejidad de un sistema
+
 # Aca lo mismo, el usuario conduce el auto pero no sabe que estamos checkeando que el auto este encendido
 
 class Auto():
