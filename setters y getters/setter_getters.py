9db9ddfb55218e4dcf92d385514991aa762bb3e8
RecamFx <camiloreartesgm@gmail.com>
2024-04-09 16:37:22 -0300

    Automatic Terminal

--- setters y getters/setter_getters.py
+++ setters y getters/setter_getters.py
@@ -13,6 +13,9 @@ class Persona:
     def get_edad2(self):
         return self.__edad2
     
+    def set_edad(self, edad):
+        self._edad = edad
+    
 stephen = Persona("Stephen", "36", "34")
 
 print(stephen._nombre) # Esto esta mal #? Stephen
@@ -21,3 +24,9 @@ print(stephen.get_nombre()) # Esto es correcto #? Stephen
 
 #!print(stephen.__edad2) # Esto esta mal #? ERROR
 print(stephen.get_edad2()) # Esto es correcto #? Stephen
+
+
+
+print(stephen._edad) #? 36
+stephen.set_edad(20) # Cambiandome de nombre
+print(stephen._edad) #? 20
